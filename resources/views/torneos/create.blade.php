b77a2bca4ca5683fc659c8d3c0bb0a4501444d2e
Laura <anoobmedic@gmail.com>
2024-05-29 08:47:37 +0200

    Añadido vista de torneos para crear torneos

--- resources/views/torneos/create.blade.php
+++ resources/views/torneos/create.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 <br>
-<h1 class="text-center text-muted bg-info-subtle">Crear Tenista</h1>
+<h1 class="text-center text-muted bg-info-subtle">Crear Torneo</h1>
 <div class="text-center">
     <img width="110px" src="{{ asset('images/raquetass.png') }}" alt="raquetas"/>
 </div>
@@ -35,95 +35,61 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-6">
-            <img src="{{ asset('images/RafaLado.jpg') }}" alt="Rafael" class="img-fluid"/>
-            <br><br><br>
-            <img width="600px" src="{{ asset('images/atp_tour_2024.png') }}" alt="Logo" />
+            <img src="{{ asset('images/torneosProximos.webp') }}" alt="Tenis" class="img-fluid"/>
         </div>
         <div class="col-md-6">
-            <form action="{{ route("tenistas.store") }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route("torneos.store") }}" method="post" enctype="multipart/form-data">
                 @csrf
-
                 <div class="form-group m-3">
-                    <label for="nombre">Nombre:</label>
-                    <input class="form-control" id="nombre" name="nombre" type="text" required>
+                    <label for="ubicacion">Ubicación</label>
+                    <input class="form-control" id="ubicacion" name="ubicacion" type="text" required>
                 </div>
 
                 <div class="form-group m-3">
-                    <label for="pais">Pais:</label>
-                    <input class="form-control" id="pais" name="pais" type="text" required>
-                </div>
-
-                <div class="form-group m-3">
-                    <label for="fecha_nacimiento">Fecha de nacimiento:</label>
-                    <input class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" type="date" required>
-                </div>
-
-                <div class="form-group m-3">
-                    <label for="edad">Edad:</label>
-                    <input class="form-control" id="edad" name="edad" type="number" required>
-                </div>
-
-                <div class="form-group m-3">
-                    <label for="altura">Altura:</label>
-                    <input class="form-control" id="altura" name="altura" type="number" required>
-                </div>
-
-                <div class="form-group m-3">
-                    <label for="peso">Peso:</label>
-                    <input class="form-control" id="peso" name="peso" type="number" required>
-                </div>
-
-                <div class="form-group m-3">
-                    <label for="fecha_debut">Fecha Debut:</label>
-                    <input class="form-control" id="fecha_debut" name="fecha_debut" type="date" required>
-                </div>
-
-                <div class="form-group m-3">
-                    <label for="mano">Mano:</label>
+                    <label for="modalidad">Modalidad</label>
                     <br>
-                    <label class="text-muted"> Diestro, Zurdo </label>
-                    <input class="form-control" id="mano" name="mano" type="text" required>
+                    <label class="text-muted"> Individual, Dobles o Individual y Dobles</label>
+                    <input class="form-control" id="modalidad" name="modalidad" type="text" required>
                 </div>
-
                 <div class="form-group m-3">
-                    <label for="reves">Revés:</label>
+                    <label for="categoria">Categoria</label>
                     <br>
-                    <label class="text-muted"> una_mano, dos_manos </label>
-                    <input class="form-control" id="reves" name="reves" type="text" required>
+                    <label class="text-muted">Master 1000, Master 200 o Master 350</label>
+                    <input class="form-control" id="categoria" name="categoria" type="text" required>
                 </div>
-
                 <div class="form-group m-3">
-                    <label for="dinero_ganado">Dinero Ganado:</label>
-                    <input class="form-control" id="dinero_ganado" name="dinero_ganado" type="number" required>
+                    <label for="superficie">Superficie</label>
+                    <br>
+                    <label class="text-muted">Duro, hierba o Arcilla</label>
+                    <input class="form-control" id="superficie" name="superficie" type="text" required>
                 </div>
-
                 <div class="form-group m-3">
-                    <label for="mejor_ranking">Mejor Rank:</label>
-                    <input class="form-control" id="mejor_ranking" name="mejor_ranking" type="text" required>
+                    <label for="vacantes">Vacantes</label>
+                    <br>
+                    <label class="text-muted">Añadir ID del tenista que pueda participar</label>
+                    <input class="form-control" id="vacantes" name="vacantes" type="text" required>
                 </div>
-
                 <div class="form-group m-3">
-                    <label for="entrenador">Entrenador:</label>
-                    <input class="form-control" id="entrenador" name="entrenador" type="text" required>
+                    <label for="premios">Premios</label>
+                    <br>
+                    <label class="text-muted">10.000.000 por ejemplo</label>
+                    <input class="form-control" id="premios" name="premios" type="text" required>
                 </div>
-
                 <div class="form-group m-3">
-                    <label for="num_victorias">Numero de Victorias:</label>
-                    <input class="form-control" id="num_victorias" name="num_victorias" type="number" required>
+                    <label for="fecha_inicio">Fecha Inicio</label>
+                    <input class="form-control" id="fecha_inicio" name="fecha_inicio" type="date" required>
                 </div>
-
                 <div class="form-group m-3">
-                    <label for="num_derrotas">Numero de Derrotas:</label>
-                    <input class="form-control" id="num_derrotas" name="num_derrotas" type="number" required>
+                    <label for="fecha_fin">Fecha Fin</label>
+                    <input class="form-control" id="fecha_fin" name="fecha_fin" type="date" required>
                 </div>
-
                 <div class="form-group m-3">
                     <label for="imagen">Imagen:</label>
                     <input class="form-control" id="imagen" name="imagen" type="file" required>
                 </div>
 
                 <button class="btn btn-primary m-3" type="submit">Crear</button>
-                <a class="btn btn-secondary m-3" href="{{ route('tenistas.index') }}">Volver</a>
+                <a class="btn btn-secondary m-3" href="{{ route('torneos.index') }}">Volver</a>
             </form>
         </div>
     </div>
