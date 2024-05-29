e76a8ca4756ab8a042b814b13ee5c6a520b19fd7
Laura <anoobmedic@gmail.com>
2024-05-29 11:04:22 +0200

    Detalles de Torneos vistas terminado

--- resources/views/torneos/show.blade.php
+++ resources/views/torneos/show.blade.php
@@ -21,7 +21,22 @@
     <dl class="row text-muted  row-cols-md-6 row-cols-1">
         <dt class="col-sm-3">ID</dt>
         <dd class="col-sm-9">{{ $torneos->id }}</dd>
-        <dt class="col-sm-3">Nombre</dt>
+        <dt class="col-sm-3">Ubicación</dt>
+        <dd class="col-sm-9">{{ $torneos->ubicacion }}</dd>
+        <dt class="col-sm-3">Modalidad</dt>
+        <dd class="col-sm-9">{{ $torneos->modalidad }}</dd>
+        <dt class="col-sm-3">Categoria</dt>
+        <dd class="col-sm-9">{{ $torneos->categoria }}</dd>
+        <dt class="col-sm-3">Superficie</dt>
+        <dd class="col-sm-9">{{ $torneos->superficie }}</dd>
+        <dt class="col-sm-3">Vacantes</dt>
+        <dd class="col-sm-9">{{ $torneos->vacantes }}</dd>
+        <dt class="col-sm-3">Premios</dt>
+        <dd class="col-sm-9">{{ $torneos->premios }}</dd>
+        <dt class="col-sm-3">Fecha Inicio</dt>
+        <dd class="col-sm-9">{{ $torneos->fecha_inicio }}</dd>
+        <dt class="col-sm-3">Fecha Fin</dt>
+        <dd class="col-sm-9">{{ $torneos->fecha_fin }}</dd>
         <dt class="col-sm-3">Imagen</dt>
         <dd class="col-sm-9"><img src="{{ $torneos->imagen }}" alt="{{ $torneos->nombre }}" width="150"></dd>
     </dl>
@@ -30,7 +45,7 @@
 <a class="btn btn-primary text-white" href="{{route('torneos.index')}}">Volver</a>
 </body>
 <br> <br>
-<img height="700px" width="1490px" src="{{ asset('images/tenistasDetalles.png') }}" alt="GifTenistas" />
+<img height="700px" width="1490px" src="{{ asset('images/atpCollage.png') }}" alt="GifTenistas" />
 @include('about')
 @include('footer')
 </html>
